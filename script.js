cc5cbace18951339342d9db6cff249324f9bc42d
Alyssa <alyssaw90@gmail.com>
2015-03-13 16:10:21 -0700

    search function working

--- script.js
+++ script.js
@@ -7,6 +7,43 @@ moviesApp.controller('SearchController', ['$scope', '$http', function($scope,$ht
   $scope.loading = false;
   $scope.searchTerms = [];
 
+  try {
+    $scope.searchTerms = JSON.parse(window.localStorage.searchTerms) || [];
+  } catch(e){
+    console.log('error', e);
+    $scope.searchTerms = [];
+  }
+
+  $scope.search = function(){
+    $scope.loading = true;
+
+    if($scope.searchTerm.length < 1){
+      $scope.movies.Error = "Must provide more than one character"
+      return;
+    }
+
+    var req = {
+      url: 'http://www.omdbapi.com',
+      params: {
+        s: $scope.searchTerm,
+        type: $scope.searchType
+      }
+    }
+
+    window.localStorage.searchTerms = JSON.stringify($scope.searchTerms);
+
+    $http(req).success(function(data){
+      if($scope.searchTerms.indexOf($scope.searchTerm) === -1){
+        $scope.searchTerms.push($scope.searchTerm);
+      }
+      $scope.movies = data;
+      $scope.loading = false
+    });
+  }
+
+  if($scope.searchTerm){
+    $scope.search();
+  }
 }]);
 
 moviesApp.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
