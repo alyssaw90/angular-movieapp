d6ea88f7d26b46ff2a4ec31800d9ff3ac6e06a0f
Alyssa <alyssaw90@gmail.com>
2015-03-12 22:36:52 -0700

    search form setup

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 var moviesApp = angular.module('MoviesApp', ['ui.bootstrap', 'ngMessages', 'ngRoute'])
+
+moviesApp.controller('SearchController', ['$scope', '$http', function($scope,$http){
+
+  $scope.movies={}
+  $scope.searchTerm = '';
+  $scope.loading = false;
+  $scope.searchTerms = [];
+
+}]);
+
+moviesApp.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/search.html',
+      controller: 'SearchController'
+    })
+
+}]);
